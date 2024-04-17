3cfad2c1c1e073005f9b3be017a1779b24d6f82d
h25jyeon <h25jy0908@gmail.com>
2024-04-17 09:46:49 +0900

    workign set 지울 때 해단 patter data도 지워지도록 추가

--- licenseclassify/src/main/java/com/gytni/licenseclassify/repo/ProductPatternRepo.java
+++ licenseclassify/src/main/java/com/gytni/licenseclassify/repo/ProductPatternRepo.java
@@ -3,13 +3,23 @@ package com.gytni.licenseclassify.repo;
 import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import com.gytni.licenseclassify.model.ProductPattern;
+
+import jakarta.transaction.Transactional;
 
 public interface ProductPatternRepo extends CrudRepository<ProductPattern, UUID> {
     List<ProductPattern> findByWorkingSetId(UUID id);
     List<ProductPattern> findByUnclassified(boolean unclassified);
     List<ProductPattern> findByWorkingSetIdOrderByCreatedDesc(UUID workingSetId);
     List<ProductPattern> findByWorkingSetIdAndUnclassifiedFalse(UUID workingSetId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM ProductPattern p WHERE p.workingSetId = :workingSetId")
+    void deleteByWorkingSetId(@Param("workingSetId") UUID workingSetId);
 }
